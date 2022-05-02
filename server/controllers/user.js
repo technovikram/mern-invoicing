5035470be7897f4d6b9e5fb4ea32244394ae9a7b
vbakhru <techno.vikram@gmail.com>
2022-05-02 12:31:27 +0000

    Updated to test

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -37,7 +37,7 @@ export const signin = async (req, res)=> {
         res.status(200).json({ result: existingUser, userProfile, token })
 
     } catch (error) {
-        res.status(500).json({ message: "Something went wrong"})
+        res.status(500).json({ message: "Something went wrong!"})
     }
 }
 
